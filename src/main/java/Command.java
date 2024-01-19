3c082341d0ff8de447a6dde536336444066f2df1
Adonai Hernandez Bolaños <adonai.acsii@gmail.com>
2024-01-19 21:09:50 +0000

    Create Command interface

---
+++ src/main/java/Command.java
@@ -0,0 +1,29 @@
+public interface Command {
+
+    interface Output{
+        String result();
+
+        int code();
+    }
+
+    interface Input{
+        String get(String key);
+    }
+
+    static Output output(String res, int code){
+
+        return new Output() {
+            @Override
+            public String result() {
+                return res;
+            }
+
+            @Override
+            public int code() {
+                return code;
+            }
+        };
+
+    }
+
+}
