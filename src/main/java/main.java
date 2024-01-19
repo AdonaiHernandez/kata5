031a6ac5981fce1ea6525d015d297cee561be9c1
Adonai Hernandez Bolaños <adonai.acsii@gmail.com>
2024-01-19 21:21:10 +0000

    Finish implement factorial api in main class

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,6 +7,26 @@ public class main {
 
         Spark.port(8080);
         Spark.get("/", (req, res) -> "Hola mundo!");
+        Spark.get("/factorial", (req, res) -> new CommandExec(req, res).execute(new FactorialCommand()));
+    }
+
+    private record CommandExec(spark.Request req, spark.Response res){
+
+        public String execute(Command command){
+            Command.Output output = command.execute(input());
+            res.status(output.code());
+            return output.result();
+        }
+
+        public Command.Input input(){
+            return new Command.Input() {
+                @Override
+                public String get(String key) {
+                    return req.queryParams(key) == null ? req.params(key) : req.queryParams(key);
+                }
+            };
+        }
+
     }
 
 }
